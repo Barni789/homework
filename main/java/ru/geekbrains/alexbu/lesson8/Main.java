a1dae08841aa077c6935fded773f02b537a36fff
Alex <73363294+Barni789@users.noreply.github.com>
2021-02-21 13:31:45 +0100

    Update Main.java

--- main/java/ru/geekbrains/alexbu/lesson8/Main.java
+++ main/java/ru/geekbrains/alexbu/lesson8/Main.java
@@ -1,25 +1,23 @@
 package ru.geekbrains.alexbu.lesson8;
 
 public class Main {
-    public static void main(String[] args) {
-        Player human = new Human("Павел", 45, 2);
-        Player cat = new Cat("Барсик", 500, 4);
-        Player robot = new Robot("Валли", 450, 1);
+   public static void main(String[] args) {
+        Player human = new Human("Павел", 450, 2);
+        Player cat = new Cat("Барсик", 700, 4);
+        Player robot = new Robot("Валли", 1000, 1);
         Player[] players = {human, cat, robot};
 
-        Hurdles wall1 = new Wall(90);
-        Hurdles wall2 = new Wall(500);
-        Runtrack runTrack1 = new Runtrack(10);
-        Runtrack runTrack2 = new Runtrack(2);
+        Hurdles wall1 = new Wall(3);
+        Hurdles wall2 = new Wall(5);
+        Runtrack runTrack1 = new Runtrack(1005);
+        Runtrack runTrack2 = new Runtrack(500);
         Hurdles[] hurdles = {wall1, wall2, runTrack1, runTrack2};
 
         for (int i = 0; i < hurdles.length; i++) {
-            for (Player player : players) {
-                if (player.getAttempt()) {
-                    hurdles[i].winCheck(player);
-                }
-
-
+            System.out.println("\nИспытание " + (i + 1));
+            for (int j = 0, playersLength = players.length; j < playersLength; j++) {
+                Player player = players[j];
+                hurdles[i].winCheck(player);
             }
         }
     }
